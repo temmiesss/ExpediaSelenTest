2563a8469c05ddb15dd5555276112bab880f6960
ChelovekBayan <kanalsaifera@mail.ru>
2025-01-17 17:30:14 +0600

    Need to done this later

--- src/test/java/ExpediaTest.java
+++ src/test/java/ExpediaTest.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +20,7 @@ public class ExpediaTest {
 
     @Test(description = "expedia order highest price" )
     public void testForExpedia(){
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new EdgeDriver();
         driver.get("https://www.expedia.com");
         driver.manage().deleteAllCookies();
 //        Dimension dimension = new Dimension(400,200);
@@ -37,10 +36,18 @@ public class ExpediaTest {
         driver.findElement(By.xpath("//section[@data-testid='popover-sheet']/div/div[3]/div/button")).click();
         driver.findElement(By.xpath("//button[contains(text(),'Search')]")).click();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        List<WebElement> price = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//tr[@class = 'ng-star-inserted']/td[3]/span/span")));
-        System.out.println(price);
+
+        List<WebElement> price = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[contains(@class, 'cruise-sailing-dates-container')]")));
+
+        List<String> pricelist = new ArrayList<String>();
+        int i = 0;
         for(WebElement priceElement : price){
-            System.out.println(priceElement.getText());
+            List<WebElement> priceRow = priceElement.findElements(By.xpath("//tr[@class = 'ng-star-inserted' or contains(@class,'odd ng-star-inserted')]/td[3]/span/span"));
+
+            for(WebElement priceRowElement : priceRow){
+                pricelist.add(priceRowElement.getText());
+            }
         }
+        System.out.println(pricelist);
     }
 }
